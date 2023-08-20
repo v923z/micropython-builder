0862180547d00f1670ee101f452757544fb5cf7c
Zoltán Vörös <zvoros@gmail.com>
2023-08-20 16:06:36 +0200

    try to fix preprocessor flag (#14)
    * preprocessor flag

--- scripts/rp2/rp2.sh
+++ scripts/rp2/rp2.sh
@@ -14,7 +14,9 @@ build_rp2() {
 }
 
 build_rp2_uart_vfat() {
-    make ${MAKEOPTS} -C micropython/ports/rp2 BOARD=$1 USER_C_MODULES=../../../ulab/code/micropython.cmake CFLAGS_EXTRA=-DULAB_HASH=$ulab_hash CFLAGS_EXTRA=-DMICROPY_HW_ENABLE_UART_REPL=1 CFLAGS_EXTRA=-DMICROPY_HW_USB_MSC=1
+    MICROPY_HW_ENABLE_UART_REPL=1
+    MICROPY_HW_USB_MSC=1
+    make ${MAKEOPTS} -C micropython/ports/rp2 BOARD=$1 USER_C_MODULES=../../../ulab/code/micropython.cmake CFLAGS_EXTRA=-DULAB_HASH=$ulab_hash 
     copy_files rp2/build-$1/firmware.uf2 "$1"_UART_VFAT
     clean_up rp2 build-$1
 }
