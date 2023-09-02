2a38b59a2ec9062d1ed363dc2a553090580c26ff
Zoltán Vörös <zvoros@gmail.com>
2023-09-02 18:01:53 +0200

    update ESP build

--- scripts/install_espidf.sh
+++ scripts/install_espidf.sh
@@ -3,7 +3,10 @@
 # Instructions from: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html
 sudo apt-get install git wget flex bison gperf python3 python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 
-git clone -b v4.4 --recursive https://github.com/espressif/esp-idf.git
+pip3 install pyelftools
+git clone https://github.com/espressif/esp-idf.git
+git -C esp-idf checkout v5.0.2
+./esp-idf/install.sh
 
 cd esp-idf
 ./install.sh all
