d8821398754057fa3cab7b07ead63162381f02cd
Zoltán Vörös <zvoros@gmail.com>
2023-09-02 18:58:56 +0200

    fix ESP32 release file name

--- scripts/esp32/esp32.sh
+++ scripts/esp32/esp32.sh
@@ -11,6 +11,6 @@ source ./scripts/init.sh
 build_esp32() {
     source esp-idf/export.sh
     make ${MAKEOPTS} -C micropython/ports/esp32 BOARD=$1 USER_C_MODULES=../../../../ulab/code/micropython.cmake CFLAGS_EXTRA=-DULAB_HASH=$ulab_hash
-    copy_files esp32/build-$1/firmware.bin ESP32-$1
+    copy_files esp32/build-$1/firmware.bin $1
     clean_up esp32 build-$1
 }
