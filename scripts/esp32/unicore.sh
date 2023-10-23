e6d2391ede988b200d58e36d8669655cae3d051a
Zoltán Vörös <zvoros@gmail.com>
2023-10-23 21:01:31 +0200

    restore yaml file

--- scripts/esp32/unicore.sh
+++ scripts/esp32/unicore.sh
@@ -3,11 +3,8 @@
 #  This file is part of the micropython-builder project,
 #  https://github.com/v923z/micropython-builder
 #  The MIT License (MIT)
-#  Copyright (c) 2022 Zoltán Vörös
-#                2023 Zach Moshe
-#                2023 Ganer
+#  Copyright (c) 2023 Zoltán Vörös
 
-source ./scripts/esp32/esp32.sh
 source esp-idf/export.sh
 
 make ${MAKEOPTS} -C micropython/ports/esp32 BOARD=ESP32_GENERIC BOARD_VARIANT=UNICORE USER_C_MODULES=../../../../ulab/code/micropython.cmake CFLAGS_EXTRA=-DULAB_HASH=$ulab_hash
