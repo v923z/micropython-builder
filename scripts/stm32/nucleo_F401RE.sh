37c94e7c9c9b34d6dab723889c03c0a250068620
Arthur GAUTHERON <46901597+agautheron@users.noreply.github.com>
2023-11-10 19:25:38 +0100

    Add Scripts for NUCLEO_L476RG and F401RE boards (#22)

---
+++ scripts/stm32/nucleo_F401RE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  This file is part of the micropython-builder project,
+#  https://github.com/v923z/micropython-builder
+#  The MIT License (MIT)
+#  Copyright (c) 2022 Zoltán Vörös
+
+source ./scripts/stm32/stm32.sh
+
+build_stm32 "NUCLEO_F401RE"
