692d248b487ad8c5191378fd4c7f3e55eb4feb38
Zoltán Vörös <zvoros@gmail.com>
2024-03-07 10:00:47 +0100

    add Teensy 41 script (#26)

---
+++ scripts/mimxrt/teensy41.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  This file is part of the micropython-builder project,
+#  https://github.com/v923z/micropython-builder
+#  The MIT License (MIT)
+#  Copyright (c) 2024 Zoltán Vörös
+
+source ./scripts/mimxrt/mimxrt.sh
+
+build_mimxrt "TEENSY41"
