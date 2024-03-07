c031069e9b6f096c66fc73930fba126d4e666170
Zoltán Vörös <zvoros@gmail.com>
2024-03-07 09:11:16 +0100

    add sparkfun_thing_plus

---
+++ scripts/samd/sparkfun_thing_plus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  This file is part of the micropython-builder project,
+#  https://github.com/v923z/micropython-builder
+#  The MIT License (MIT)
+#  Copyright (c) 2024 Zoltán Vörös
+
+source ./scripts/samd/samd.sh
+
+build_samd SPARKFUN_SAMD51_THING_PLUS
