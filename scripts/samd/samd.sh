1cdc4f3eb037968b6a5ec86877342592c7cb8eb6
Zoltán Vörös <zvoros@gmail.com>
2024-03-07 09:36:29 +0100

    add sparkfun_thing_plus (#25)
    * add sparkfun_thing_plus
    
    * re-insert all ports

---
+++ scripts/samd/samd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#  This file is part of the micropython-builder project,
+#  https://github.com/v923z/micropython-builder
+#  The MIT License (MIT)
+#  Copyright (c) 2024 Zoltán Vörös
+
+source ./scripts/init.sh
+
+build_samd() {
+    make ${MAKEOPTS} -C micropython/ports/samd BOARD=$1 USER_C_MODULES=../../../ulab all CFLAGS_EXTRA="-DULAB_HASH_STRING=$ulab_hash -DULAB_SUPPORTS_COMPLEX=0"
+    copy_files samd/build-$1/firmware.uf2 $1
+    clean_up samd build-$1
+}
