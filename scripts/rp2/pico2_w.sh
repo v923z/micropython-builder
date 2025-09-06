51789e4ccb2cd08e84c11a069278ab691cdddefb
Jay-Damodaran <146678205+Jay-Damodaran@users.noreply.github.com>
2025-09-06 18:10:07 -0500

    Update pico2_w.sh

--- scripts/rp2/pico2_w.sh
+++ scripts/rp2/pico2_w.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#  This file is part of the micropython-builder project,
+#  https://github.com/v923z/micropython-builder
+#  The MIT License (MIT)
+#  Copyright (c) 2022 Zoltán Vörös
+#  File Author: Jayanth Damodaran    Last Modified: Sept. 6, 2025
+
 source ./scripts/rp2_init.sh
 
 cd micropython/ports/rp2
