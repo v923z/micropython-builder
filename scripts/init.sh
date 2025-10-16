b36f2fc7c663b1198d7b0ec80a4780f896fedb54
Zoltán Vörös <zvoros@gmail.com>
2025-10-16 22:22:15 +0200

    hard-wire micropython version

--- scripts/init.sh
+++ scripts/init.sh
@@ -23,8 +23,8 @@ git clone https://github.com/v923z/micropython-ulab ulab || git -C ulab pull
 # only check out micropython, if it is not available locally, otherwise, pull
 git clone https://github.com/micropython/micropython micropython || git -C micropython pull
 : ${MICROPYTHON_TAG:=$(git tag --sort -v:refname | grep -v preview | head -n 1)}
-MICROPYTHON_TAG = 'v1.24.0'
-git -C micropython checkout ${MICROPYTHON_TAG}
+# git -C micropython checkout ${MICROPYTHON_TAG}
+git -C micropython checkout v1.24.0
 
 cd micropython
 git submodule update --init
